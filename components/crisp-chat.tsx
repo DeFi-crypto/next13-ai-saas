0bef10ef4599bb0f2f2b9b7ab88d372fc76e6239
Defi-Crypto <subtorandomstuff@gmail.com>
2024-02-24 01:55:55 -0600

    Updated crisp

--- components/crisp-chat.tsx
+++ components/crisp-chat.tsx
@@ -5,7 +5,7 @@ import { Crisp } from "crisp-sdk-web";
 
 export const CrispChat = () => {
   useEffect(() => {
-    Crisp.configure("8254f25a-eca0-421e-b4de-2bf655c03887");
+    Crisp.configure("d12ccc6a-aee8-4c32-90a8-6bcf30aa78f4");
   }, []);
 
   return null;
